a287178a3dd0b16b9abae630b60dc78ff43d94ab
migheljackson <10728980+migheljackson@users.noreply.github.com>
2021-06-23 00:33:42 -0500

    Added list of departements and managers to queries

--- emp_quereies.sql
+++ emp_quereies.sql
@@ -9,3 +9,10 @@ select e.first_name,e.last_name,e.hire_date from employees as e
 where hire_date>'1985-12-31'
 and hire_date<'1987-1-1';
 
+--List departments and names of all managers
+select d.dept_no,d.dept_name,dm.emp_no,e.last_name,e.first_name
+from departments as d
+inner join dept_manager as dm
+on d.dept_no=dm.dept_no
+inner join employees as e
+on dm.emp_no=e.emp_no;
